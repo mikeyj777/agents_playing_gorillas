bf26d93350944bbf57434e26d47c5d36997fa83f
Michael James <mikeyj777@gmail.com>
2024-09-13 22:57:00 -0400

    small changes

--- gorilla.py
+++ gorilla.py
@@ -7,13 +7,14 @@ from agent import Agent
 from trajectory import trajectory_with_drag
 
 class Gorilla:
-    def __init__(self):
+    def __init__(self, upwind=True):
         self.id = f'{uuid.uuid4()}'
         self.height = random.randint(1, 100)  # Gorilla height (1 to 100 units)
         self.x_coord = random.randint(0, Consts.X_BOUNDS['max'])
         self.state = {}
         self.target_y = None
         self.agent = Agent(hyperparameter_set='gorillas', gorilla=self)
+        self.upwind = upwind
     
     def get_action(self):
 
--- gorillas_game.py
+++ gorillas_game.py
@@ -24,30 +24,13 @@ class Gorillas_Game:
     def __init__(self, num_players = 2):
         self.num_players = num_players
         self.players = []
+        upwind = True
         for _ in range(self.num_players):
-            self.players.append(Gorilla())
-
-    def get_y_at_closest_x(self, traj_dataset, distance):
-        min_dist = np.inf
-        y_at_closest_x = -1
-        closest_x = np.inf
-        for _, row in traj_dataset.iterrows():
-            x = row['position_x']
-            y = row['position_y']
-            curr_dist = abs(x-distance)
-            if curr_dist < min_dist:
-                min_dist = curr_dist
-                y_at_closest_x = y
-                closest_x = x
-    
-        if abs(closest_x - distance) > Consts.IMPACT_TOLERANCE:
-            y_at_closest_x = None
-
-        return y_at_closest_x
+            self.players.append(Gorilla(upwind=upwind))
+            upwind = not upwind
 
     def start_the_action(self, player):
         player.get_action()
-
 
     def initialize_game(self):
         self.wind_speed = random.randint(0, 10)
@@ -79,3 +62,7 @@ class Gorillas_Game:
             self.players = [self.g1, self.g2]
             with multiprocessing.Pool() as p:
                 _ = p.map(self.start_the_action, self.players)
+
+if __name__ == '__main__':
+    game = Gorillas_Game()
+    game.play_game()
